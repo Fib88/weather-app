88ee92fa1176b9400c5a0c7c01fd33df8448c16c
Fib88 <nsukifrank@hotmail.com>
2020-09-16 21:05:50 +0200

    got rid of redundant code

--- index.js
+++ index.js
@@ -4,44 +4,23 @@ let mykey = config.SECRET_KEY;
 
     document.getElementById("run").addEventListener("click", function(){
 
-        function getWeather(){
-            const input = document.getElementById("input").value
-
-
-            fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=" +mykey)
-                .then(response => response.json())
-                .then(data =>{
-                    console.log(data);
-                    let Name = data.name;
-                    let country = data.sys.country;
-                    let timeZone = data.timezone;
-                    let temperature = data.main.temp;
-                    let description = data.weather[0].description;
-
-                    let weatherShow = "City Name: " + Name + " Country: " + country + " Time Zone: " + timeZone + " " + "Degrees Celcius "+ temperature + " description: " + description;
-                    showWeather(weatherShow);
-
-                })
-        }
-        getWeather();
-
         function getWeather5Days(){
           let input = document.getElementById("input").value;
 
-            fetch("https://api.openweathermap.org/data/2.5/forecast?q="+input+"&units=metric&appid="+ mykey)
+            fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ input +"&units=metric&appid="+ mykey)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data);
-                    let temp = data.list.main.temp;
-                    let description5Days = data.list.weather.description;
-                    let weatherShow5Days = "Temperature " + temp + "Conditions " + description5Days;
+                    let name = data.city.name;
+                        console.log(name);
+                    let weatherShow5Days = name;
                     showWeather(weatherShow5Days)
                 })
         }
         getWeather5Days();
 
         function showWeather(weatherShow,weatherShow5Days){
-           document.getElementById("target").innerHTML = weatherShow;
+           //document.getElementById("target").innerHTML = weatherShow;
            document.getElementById("target2").innerHTML = weatherShow5Days;
         }
         showWeather();
@@ -53,11 +32,28 @@ let mykey = config.SECRET_KEY;
 
 
 
-    // function showArray(description){
-    //     let arrayPrint = [];
-    //     for ( let i = 0; i < description.length; i++) {
-    //         arrayPrint = arrayPrint.append(description[i]);
-    //     }
-    //     console.log(showArray());
-    // }
-    // showArray();
+
+
+
+
+
+// function getWeather(){
+//     const input = document.getElementById("input").value
+//
+//
+// //     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=" +mykey)
+// //         .then(response => response.json())
+// //         .then(data =>{
+// //             console.log(data);
+// //             let Name = data.name;
+// //             let country = data.sys.country;
+// //             let timeZone = data.timezone;
+// //             let temperature = data.main.temp;
+// //             let description = data.weather[0].description;
+// //
+// //             let weatherShow = "City Name: " + Name + " Country: " + country + " Time Zone: " + timeZone + " " + "Degrees Celcius "+ temperature + " description: " + description;
+// //             showWeather(weatherShow);
+// //
+// //         })
+// // }
+// // getWeather();
