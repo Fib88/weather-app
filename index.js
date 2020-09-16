229d6af441d4309bb4d3bea63fbc7011e67e7eed
Fib88 <nsukifrank@hotmail.com>
2020-09-16 14:30:15 +0200

    added country to info to display still need to figure out how to do it for 5days

--- index.js
+++ index.js
@@ -13,18 +13,34 @@ let mykey = config.SECRET_KEY;
                 .then(data =>{
                     console.log(data);
                     let Name = data.name;
-                    let id = data.id;
+                    let country = data.sys.country;
                     let timeZone = data.timezone;
                     let temperature = data.main.temp;
-                    let weatherShow = "City Name: " + Name + " City ID: " + id + " Time Zone: " + timeZone + " "+ temperature;
+                    let weatherShow = "City Name: " + Name + " Country: " + country + " Time Zone: " + timeZone + " " + "Degrees Celcius"+ temperature;
                     showWeather(weatherShow);
 
                 })
         }
         getWeather();
 
-        function showWeather(weatherShow){
+        function getWeather5Days(){
+            input = document.getElementById("input").value;
+
+            fetch("https://api.openweathermap.org/data/2.5/forecast?q="+input+"&units=metric&appid="+ mykey)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    let temp = data.list.main.temp;
+                    let description = data.list.weather.description;
+                    let weatherShow5Days = "Temperature " + temp + "Conditions " + description;
+                    showWeather(weatherShow5Days)
+                })
+        }
+        getWeather5Days();
+
+        function showWeather(weatherShow,weatherShow5Days){
            document.getElementById("target").innerHTML = weatherShow;
+           document.getElementById("target2").innerHTML = weatherShow5Days;
         }
         showWeather();
 
