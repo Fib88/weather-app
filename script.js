aa5dc1ffd652c0bd36d113727086878d5ee2398c
Fib88 <nsukifrank@hotmail.com>
2020-09-17 17:12:13 +0200

    managed to add image urls and used math.floor to round down temps

--- script.js
+++ script.js
@@ -24,28 +24,43 @@ document.getElementById("run").addEventListener("click", function(){
            .then(data => {
             console.log(data)
             let temperature_day0 = data.daily[0].temp.day;
+            let description_day0 = data.daily[0].weather[0].description;
+            let icon_day0 = data.daily[0].weather[0].icon;
             console.log(temperature_day0);
+            console.log(description_day0);
+            console.log(icon_day0);
             let temperature_day1 = data.daily[1].temp.day;
+            let description_day1 = data.daily[1].weather[0].description;
             console.log(temperature_day1);
+            console.log(description_day1);
             let temperature_day2 = data.daily[2].temp.day;
+            let description_day2 = data.daily[2].weather[0].description;
             console.log(temperature_day2);
+            console.log(description_day2);
             let temperature_day3 = data.daily[3].temp.day;
+            let description_day3 = data.daily[3].weather[0].description;
             console.log(temperature_day3);
+            console.log(description_day3);
             let temperature_day4 = data.daily[4].temp.day;
+            let description_day4 = data.daily[4].weather[0].description;
             console.log(temperature_day4);
+            console.log(description_day4);
 
-            showData(temperature_day0,temperature_day1,temperature_day2,temperature_day3,temperature_day4);
+            showData(temperature_day0,temperature_day1,temperature_day2,temperature_day3,temperature_day4,description_day1
+            ,description_day2,description_day3,description_day4,description_day0,icon_day0);
 
            })
    }
 
 
-    function showData(temperature_day0,temperature_day1,temperature_day2,temperature_day3,temperature_day4){
-        document.getElementById("target0").innerHTML =  temperature_day0;
-        document.getElementById("target1").innerHTML = temperature_day1;
-        document.getElementById("target2").innerHTML = temperature_day2;
-        document.getElementById("target3").innerHTML = temperature_day3;
-        document.getElementById("target4").innerHTML = temperature_day4;
+    function showData(temperature_day0,temperature_day1,temperature_day2,temperature_day3,temperature_day4,description_day1
+        ,description_day2,description_day3,description_day4, description_day0, icon_day0){
+        document.getElementById("target0").innerHTML = Math.floor(temperature_day0) + "°C "+" " +description_day0 + icon_day0;
+        document.getElementById("icon0").src = "http://openweathermap.org/img/wn/"+icon_day0+"@2x.png"
+        document.getElementById("target1").innerHTML = Math.floor(temperature_day1) + "°C "+" " +description_day1;
+        document.getElementById("target2").innerHTML = Math.floor(temperature_day2) + "°C "+" " +description_day2;
+        document.getElementById("target3").innerHTML = Math.floor(temperature_day3) + "°C" +" " +description_day3;
+        document.getElementById("target4").innerHTML = Math.floor(temperature_day4) + "°C"+" " +description_day4;
 
     }
     showData();
